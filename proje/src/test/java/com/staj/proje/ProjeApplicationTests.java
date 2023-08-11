516a2a4e0a93308185b4b89044ddc07eac7d10ab
BerkayEmiralioglu <140810867+BerkayEmiralioglu@users.noreply.github.com>
2023-08-11 11:02:40 +0300

    Add files via upload

---
+++ proje/src/test/java/com/staj/proje/ProjeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.staj.proje;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
