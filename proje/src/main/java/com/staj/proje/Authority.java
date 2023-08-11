30938f894b6fdbac9d627111caa9474d63fd435f
BerkayEmiralioglu <140810867+BerkayEmiralioglu@users.noreply.github.com>
2023-08-11 11:02:05 +0300

    Delete proje directory

--- proje/src/main/java/com/staj/proje/Authority.java
+++
@@ -1,7 +0,0 @@
-package com.staj.proje;
-
-import org.springframework.security.config.annotation.web.configuration.WebSecurityConfiguration;
-
-public class Authority extends WebSecurityConfiguration {
-
-}
